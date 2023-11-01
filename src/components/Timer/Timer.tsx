9ea6b191fe0c1d36624dc7ed5188eb658efcd0f9
João Lee <joaumlimaum@gmail.com>
2023-11-01 15:14:16 -0300

    Created Clock component

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,8 +1,17 @@
+import Button from "../Button/Button";
+import Clock from "./Clock/Clock";
+
 export default function Timer () {
 
     return (
         <div>
             <p>Choose a Todo and Start the timer</p>
+            <div>
+                <Clock />
+            </div>
+            <Button>
+                Start
+            </Button>
         </div>
     )
 }
