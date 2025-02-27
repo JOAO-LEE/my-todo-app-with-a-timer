85c14df9cd1518994f1e8521885f4974443a2d78
João Lee <joaumlimaum@gmail.com>
2025-02-27 12:42:02 -0300

    feat: header layout

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,8 +1,10 @@
+import { Clover, Sun } from "@phosphor-icons/react";
 import "./Header.scss";
 export function Header() {
   return (
     <header className="header">
-      <h1>zentasker</h1>
+      <h1>zentasker <Clover className="clover-logo"/></h1>
+      <Sun className="theme-switcher"/>
     </header>
   )
 }
